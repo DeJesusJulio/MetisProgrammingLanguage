d12332cdbd96acf7b49a7954a79627e83b5f4317
DeJesusJulio <julio.dejesus7546@gmail.com>
2025-02-06 02:50:46 -0800

    NEW

--- ideas.md
+++ ideas.md
@@ -25,3 +25,17 @@
 ## Classes and objects
 - classes decleration: 'SocialDiv'
 - objects instatiation: 'Ranks'
+
+
+General view of code:
+
+numb num1:
+numb num2:
+num1 = 500:
+num2 = 600:
+
+func DivideNumbers(x,y){
+    return x/y
+}:
+
+show(DivideNumbers(num1/num2)):
--- lexer.hpp
+++ lexer.hpp
@@ -4,10 +4,16 @@
 //Define token types
 enum class TokenType {
     //Data types
-    //Generic Numbers
+    NUMB, DECI, TEXT, LETER,
 
-    //LOOPS
-    INT,
+    //Keywords
+    FUNC, FOR, WHILE, WHEN, THEN, SOCIALDIV, RANKS, YE, NO,
+
+    //OPERATORS
+    ASSIGN, EQUAL, NOT_EQUAL, GREATER, LESSER, GREATER_EQUAL, LESSER_EQUAL, AND, OR, NOT,
+
+    //SYMBOLS
+    SEMICOLON, LPAREN, RPAREN, LBRAC, RBRAC,
 
     //END OF FILE
     END_OF_FILE,
